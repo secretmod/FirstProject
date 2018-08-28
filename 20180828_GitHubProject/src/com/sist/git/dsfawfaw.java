90457a218ee93f15d6b5c36fefacc84d4b9adde3
LSH <LSH@sist52>
2018-08-28 15:43:32 +0900

    asd

---
+++ 20180828_GitHubProject/src/com/sist/git/dsfawfaw.java
@@ -0,0 +1,12 @@
+package com.sist.git;
+
+public class dsfawfaw {
+
+	public static void main(String[] args) {
+		System.out.println("two");
+		System.out.println("two");
+		System.out.println("two");
+
+	}
+
+}
