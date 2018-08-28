10065821d5084d4410392618f291322f425736e6
LSH <LSH@sist52>
2018-08-28 15:30:19 +0900

    dsfs

--- 20180828_GitHubProject/src/com/sist/git/MainClass.java
+++ 20180828_GitHubProject/src/com/sist/git/MainClass.java
@@ -4,6 +4,17 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
+		System.out.println("Hello Git!!");
+		
+		
+		System.out.println("Hello Git!!");
+		System.out.println("Hello Git!!");
+		System.out.println("Hello Git!!");
+		System.out.println("Hello Git!!");
+		System.out.println("Hello Git!!");
+		System.out.println("Hello Git!!");
+		System.out.println("Hello Git!!");
+		System.out.println("Hello Git!!");
 
 	}
 
